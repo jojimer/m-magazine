1e761692610028391ad27cf38325caeadaccc53b
= <=>
2021-08-22 10:22:19 +0800

    add field report

--- resources/views/components/field-report/feed.blade.php
+++ resources/views/components/field-report/feed.blade.php
@@ -1,8 +1,42 @@
 <div class="field-report-block">
 	<div class="content-preview" style="background-image: url(@asset('./images/zion-canyon1.jpg'));">
-		content preview
+		<div class="fr-main-info-wrap">
+			<div class="profile-picture">
+				<img src="@asset('./images/pp.png')" alt="pp">
+			</div>				
+			<div class="fr-info">
+				<p class="h2">Zion Canyon</p>
+				<p class="h5 font-weight-bold">Simon Crew</p>
+				<p class="font-weight-light">Author</p>
+			</div>
+			<div class="fr-date">
+				<p class="font-weight-light">
+					August 18, 2021
+				</p>
+			</div>
+		</div>
 	</div>
 	<div class="content-meta-info">
-		meta info
+		<div class="content-excerpt">
+			<p class="font-weight-bold">Excerpt</p>
+			<p class="text-left mb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis incidunt, amet assumenda, rerum error quam! Eaque placeat ipsum assumenda est doloremque distinctio officia... <span class="fr-see-more">See More</span></p>
+		</div>
+		<div class="content-tags">
+			<p class="font-weight-bold">Tags</p>
+			<p class="fr-tags"><span>#Canyon</span> <span>#National Park</span></p>
+		</div>
+		<div class="content-images-preview">
+			<p class="font-weight-bold">More Images</p>
+			<ul>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon1.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon2.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon3.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon4.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon5.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon6.jpg')" alt="zion-canyon"></li>
+				<li><img class="img-prev" src="@asset('./images/zion-canyon7.jpg')" alt="zion-canyon"></li>
+				<li>View More</li>
+			</ul>
+		</div>
 	</div>
 </div>
